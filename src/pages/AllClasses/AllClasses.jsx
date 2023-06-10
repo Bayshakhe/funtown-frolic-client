b61212a885f4b32f6254c637ce90455f0417bd08
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-06-10 22:33:00 +0600

    redirect user for login before select class by swal

--- src/pages/AllClasses/AllClasses.jsx
+++ src/pages/AllClasses/AllClasses.jsx
@@ -1,15 +1,39 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../components/SectionTitle";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
 
 const AllClasses = () => {
+  const { user } = useAuth();
   const [danceClasses, setDanceClasses] = useState([]);
+  const navigate = useNavigate();
   // console.log(danceClasses)
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/classes`)
       .then((res) => res.json())
       .then((data) => setDanceClasses(data));
   }, []);
+
+  const handleSelect = () => {
+    if (!user) {
+      Swal.fire({
+        title: "You have to log in before select this",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/dashboard/selectedClass");
+        }
+      });
+    }
+    else{
+      navigate("/dashboard/selectedClass");
+    }
+  };
+
   return (
     <>
       <SectionTitle
@@ -23,22 +47,22 @@ const AllClasses = () => {
               <img className="h-56" src={i.classImg} alt="Dance class" />
             </figure>
             <div className="card-body">
-            <h2 className="card-title font-bold text-2xl">{i.className}</h2>
-            <div className="lg:flex justify-between items-center">
-              <h2 className="card-title text-lg">
-                {i.instructorName}
-              </h2>
-              <p className="opacity-60 lg:text-right">{i.instructorEmail}</p>
+              <h2 className="card-title font-bold text-2xl">{i.className}</h2>
+              <div className="lg:flex justify-between items-center">
+                <h2 className="card-title text-lg">{i.instructorName}</h2>
+                <p className="opacity-60 lg:text-right">{i.instructorEmail}</p>
+              </div>
+              <p className="text-teal font-semibold">Price: ${i.price}</p>
+              <div className="flex justify-between">
+                <p>Enrolled: {i.enrolled}</p>
+                <p className="text-right">Available Seat: {i.available_seat}</p>
+              </div>
+              <div className="card-actions justify-center mt-5">
+                <Link onClick={handleSelect} className="button">
+                  Select
+                </Link>
+              </div>
             </div>
-            <p className="text-teal font-semibold">Price: ${i.price}</p>
-            <div className="flex justify-between">
-              <p>Enrolled: {i.enrolled}</p>
-              <p className="text-right">Available Seat: {i.available_seat}</p>
-            </div>
-            <div className="card-actions justify-center mt-5">
-              <Link to={'/dashboard/selectedClass'} className="button">Select</Link>
-            </div>
-          </div>
           </div>
         ))}
       </div>
